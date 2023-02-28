8d30c96180121689edbacf783ddff71024c0c18e
Marcos <marcos.guibson@precojusto.app.br>
2023-02-27 23:58:25 -0400

    Creating component of loading

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { Groups } from '@screens/Groups';
 import theme from './src/theme'
 import { ThemeProvider } from 'styled-components';
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
-import { ActivityIndicator } from 'react-native';
+import { Loading } from '@components/Loading';
 
 
 export default function App() {
@@ -11,7 +11,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {!fontsLoaded ? <Groups /> : <ActivityIndicator/>}
+      {!fontsLoaded ? <Groups /> : <Loading/>}
     </ThemeProvider>
   );
 }
