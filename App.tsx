d9723e567ffdbcd70555d17b2709354b2c5d3c85
Marcos <marcos.guibson@precojusto.app.br>
2023-02-27 23:49:07 -0400

    CSS-in-JS - Using component loading

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { Groups } from '@screens/Groups';
 import theme from './src/theme'
 import { ThemeProvider } from 'styled-components';
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+import { ActivityIndicator } from 'react-native';
 
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {!fontsLoaded ? <Groups /> : <ActivityIndicator/>}
     </ThemeProvider>
   );
 }
