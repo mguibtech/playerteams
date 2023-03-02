95c7523a2354fa59738c175cb09e18b59aa755c9
mguibtech <mguibtech@gmail.com>
2023-03-02 02:51:01 -0300

    Listing groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,14 +4,27 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { StyleSheet, Text, View } from 'react-native';
 import { Container } from './styles';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+
+  const [groups, setGroups] = useState<string[]>(['Preco Justo', 'Grupo Mucuripe']);
+
   return (
     <Container>
       <Header/>
       <Highlight title='Turmas' subtitle='Jogue com a sua turma'/>
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        style={{width: '100%'}}
+        renderItem={({item}) => (
+          <GroupCard title={item}/>
+        )}
+      />
+
       
-      <GroupCard title='Traga do Preço Justo'/>
     </Container>
   );
 }
