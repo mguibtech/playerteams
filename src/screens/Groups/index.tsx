6b4f495ae2d446de997e331718c30ade6ae1a532
mguibtech <mguibtech@gmail.com>
2023-03-02 03:31:41 -0300

    Creating button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -7,6 +7,7 @@ import { Container } from './styles';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
 
@@ -27,6 +28,10 @@ export function Groups() {
         ListEmptyComponent={() => (
           <ListEmpty  message="Que tal cadastrar a primeira turma?"/>
         )}
+      />
+
+      <Button 
+        title='Criar nova turma'
       />
 
       
