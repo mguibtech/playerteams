3c2185df736860be1d97aa313355576171794e42
mguibtech <mguibtech@gmail.com>
2023-03-03 03:53:45 -0300

    Creating interface newGroup

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,8 +6,8 @@ type Props = TouchableOpacityProps & {
     type?: ButtonTypeStyleProps;
 }
 
-export function Button({title, type = 'PRIMARY', ...rest}:Props){
-    return(
+export function Button({ title, type = 'PRIMARY', ...rest }: Props) {
+    return (
         <Container type={type} {...rest}>
             <Title>{title}</Title>
         </Container>
