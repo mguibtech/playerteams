e298b8aeac62a5c69f0a6c22cb6ad63bf1e3acd3
mguibtech <mguibtech@gmail.com>
2023-03-07 04:05:17 -0300

    Implementing function back

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,8 +3,16 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { Container, Content, Icon } from "./styles";
+import {useNavigation} from '@react-navigation/native'
 
 export function NewGroup(){
+
+    const navigation = useNavigation();
+
+    function handleNew(){
+        navigation.navigate('players', {group: 'Preco Justo'})
+    }
+
     return(
         <Container>
            <Header showBackButton /> 
@@ -17,7 +25,7 @@ export function NewGroup(){
                 <Input 
                     placeholder="Nome da turma"
                 />
-                <Button title="Criar" style={{marginTop:20}}/>
+                <Button onPress={handleNew} title="Criar" style={{marginTop:20}}/>
             </Content>
         </Container>
     )
