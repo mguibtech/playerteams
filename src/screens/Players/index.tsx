00c80cde57e0a2902d48a006ff198aa976d960a5
mguibtech <mguibtech@gmail.com>
2023-03-07 02:04:16 -0300

    Creating list of players and styles for list

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -8,11 +8,14 @@ import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import {useState} from 'react'
 
 import { FlatList } from "react-native";
+import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Players() {
 
     const [team, setTeam] = useState('Time A');
-    const [players, setPlayers] = useState(['Guibson', 'Silva', 'Ana'])
+    const [players, setPlayers] = useState([])
 
 
     return (
@@ -49,7 +52,27 @@ export function Players() {
                 <NumberOfPlayers>{players.length}</NumberOfPlayers>
             </HeaderList>
 
+            <FlatList
+                data={players}
+                keyExtractor={item => item}
+                renderItem={({item }) => (
+                    <PlayerCard 
+                        onRemove={() => {}} 
+                        name={item}/>
+                )}
+                ListEmptyComponent={() => (
+                    <ListEmpty  
+                        message="Não há mais pessoas nesse time?"
+                    />
+                )}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={[
+                    { paddingBottom: 100 },
+                    players.length === 0 && { flex: 1}
+                ]}
+            />
 
+            <Button title="Teste"/>
         </Container>
     )
 }
