795b67d9459cae60928f9bbee29b01a22fa049c7
mguibtech <mguibtech@gmail.com>
2023-03-07 01:40:56 -0300

    Creating list and filter of team

---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,19 @@
+import { Container, FilterStyleProps, Title } from "./styles";
+import {TouchableOpacityProps} from 'react-native'
+
+type Props = TouchableOpacityProps & FilterStyleProps & {
+    title: string;
+}
+
+export function Filter({title, isActive = false, ...rest} : Props){
+    return(
+        <Container 
+            {...rest}
+            isActive={isActive}
+            >
+            <Title>
+                {title}
+            </Title>
+        </Container>
+    )
+}
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,21 +1,55 @@
 import { ButtonIcon } from "@components/ButtonIcon";
+import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
-import { Container } from "./styles";
+import { Input } from "@components/Input";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
-export function Players(){
-    return(
+import {useState} from 'react'
+
+import { FlatList } from "react-native";
+
+export function Players() {
+
+    const [team, setTeam] = useState('Time A');
+    const [players, setPlayers] = useState(['Guibson', 'Silva', 'Ana'])
+
+
+    return (
         <Container>
-            <Header showBackButton/>
+            <Header showBackButton />
 
-            <Highlight 
+            <Highlight
                 title="Nome da turma"
-                subtitle="Adicione a galera e serapre os times" 
+                subtitle="Adicione a galera e serapre os times"
             />
+            <Form>
+                <Input
+                    placeholder="Nome da pessoa"
+                    autoCorrect={false}
+                />
 
-            <ButtonIcon/>
+                <ButtonIcon icon="add" />
+            </Form>
 
-            
+            <HeaderList>
+
+                <FlatList
+                    data={['Time A', 'Time B']}
+                    keyExtractor = {item => item}
+                    renderItem={({item}) => (
+                        <Filter
+                            title={item} 
+                            isActive={item === team}  
+                            onPress={() => setTeam(item)}                
+                        />
+                    )}
+                    horizontal
+                />
+                <NumberOfPlayers>{players.length}</NumberOfPlayers>
+            </HeaderList>
+
+
         </Container>
     )
 }
